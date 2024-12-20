680fbe8ffd8dd9ba627b234d79af635e1695ccba
Allanwn <allanlopez201930494@cunoc.edu.gt>
2024-12-19 19:43:18 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/proyect1/BackEnd/util/Mensajes.java
@@ -0,0 +1,9 @@
+package com.mycompany.proyect1.BackEnd.util;
+
+public class Mensajes {
+
+
+    
+
+    
+}
---
+++ src/main/java/com/mycompany/proyect1/Proyect1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyect1;
+
+import com.mycompany.proyect1.BackEnd.util.Util;
+import com.mycompany.proyect1.FrontEnd.Login;
+
+/**
+ *
+ */
+public class Proyect1 {
+    
+
+    public static void main(String[] args) {
+
+    
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+
+               new Login().setVisible(true);
+            }
+        });
+
+        
+
+    }
+ 
+}
